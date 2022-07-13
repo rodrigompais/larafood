2e58a600348489d907f1184881dde83e3f9cf4de
rodrigompais <rpais@outlook.com>
2022-07-13 08:55:44 -0300

    Detalhe do Plano
    Detalhe do Plano

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,40 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+    <h1>Planos <a href="{{ route('plans.create') }}" class="btn btn-dark">ADD</a></h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            #filtros
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th style="width: 50px">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                            <td>{{ $plan->name }}</td>
+                            <td>{{ number_format($plan->price,2,',','.') }}</td>
+                            <td style="width: 10px">
+                                <a href="{{ route('plans.show', $plan->url) }}" class="btn btn-warning">VER</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            {!! $plans->links() !!}
+        </div>
+    </div>
+@stop
---
+++ resources/views/admin/pages/plans/show.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', "Detalhes do Plano {$plan->name}")
+
+@section('content_header')
+    <h1> Detalhes do Plano <b>{{$plan->name}}</b></h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <ul>
+                <li>
+                    <strong>Nome:</strong> {{$plan->name}}
+                </li>
+                <li>
+                    <strong>URL:</strong> {{$plan->url}}
+                </li>
+                <li>
+                    <strong>Preço:</strong> R$ {{ number_format($plan->price,2,',','.')}}
+                </li>
+                <li>
+                    <strong>Descição:</strong> {{$plan->description}}
+                </li>
+            </ul>
+        </div>
+    </div>
+@stop
