7fd1d053a68165704af9ffcb4a45f646bfb6e23d
rodrigompais <59613110+rodrigompais@users.noreply.github.com>
2022-08-17 23:09:23 -0300

    API NO LARAVEL - VALIDAR PEDIDO
    API NO LARAVEL - VALIDAR PEDIDO

--- app/Http/Requests/Api/StoreOrder.php
+++ app/Http/Requests/Api/StoreOrder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Api\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -24,7 +24,18 @@ class StoreOrder extends FormRequest
     public function rules()
     {
         return [
-            //
+            'token_company' => [
+                'required',
+                'exists:tenants,uuid'
+            ],
+            'table' => [
+                'nullable',
+                'exists:tables,uuid'
+            ],
+            'comment' => [
+                'nullable',
+                'max:1000',
+            ]
         ];
     }
 }
