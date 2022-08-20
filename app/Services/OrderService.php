bb5771eb52b3120f3c1bbe5ad5890084447936cb
rodrigompais <59613110+rodrigompais@users.noreply.github.com>
2022-08-20 13:09:48 -0300

    API DE PEDIDO NO LARAVEL - CALCULAR TOTAL E VALIDAR PRODUTOS
    API DE PEDIDO NO LARAVEL - CALCULAR TOTAL E VALIDAR PRODUTOS

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -3,32 +3,37 @@
 namespace App\Services;
 
 use App\Repositories\Contracts\OrderRepositoryInterface;
+use App\Repositories\Contracts\ProductRepositoryInterface;
 use App\Repositories\Contracts\TableRepositoryInterface;
 use App\Repositories\Contracts\TenantRepositoryInterface;
 use Illuminate\Http\Request;
 
 class OrderService
 {
-    protected $orderRepository, $tenantRepository, $tableRepository;
+    protected $orderRepository, $tenantRepository, $tableRepository, $productRepository;
 
     public function __construct(
         OrderRepositoryInterface $orderRepository,
         TenantRepositoryInterface $tenantRepository,
-        TableRepositoryInterface $tableRepository
-    ){
+        TableRepositoryInterface $tableRepository,
+        ProductRepositoryInterface $productRepository
+    ) {
         $this->orderRepository = $orderRepository;
         $this->tenantRepository = $tenantRepository;
         $this->tableRepository = $tableRepository;
+        $this->productRepository = $productRepository;
     }
 
     public function createNewOrder(array $order)
     {
+        $productsOrder = $this->getProductsByOrder($order['products'] ?? []);
+
         $identify = $this->getIdentifyOrder();
-        $total = $this->getTotalOrder([]);
+        $total = $this->getTotalOrder($productsOrder);
         $status = 'open';
         $tenantId = $this->getTenantIdByOrder($order['token_company']);
         $comment = isset($order['comment']) ? $order['comment'] : '';
-        $clientId = $this->getClientIdByOrder() ?? '';
+        $clientId = $this->getClientIdByOrder();
         $tableId = $this->getTableIdByOrder($order['table'] ?? '');
 
         $order = $this->orderRepository->createNewOrder(
@@ -48,10 +53,10 @@ class OrderService
     {
         $smallLetters = str_shuffle('abcdefghijklmnopqrstuvwxyz');
 
-        $numbers = (((date('Ymd')/12) * 24) + mt_rand(800, 9999));
+        $numbers = (((date('Ymd') / 12) * 24) + mt_rand(800, 9999));
         $numbers .= 1234567890;
 
-        $characters = $smallLetters.$numbers;
+        $characters = $smallLetters . $numbers;
 
         $identify = substr(str_shuffle($characters), 0, $qtyCaraceters);
 
@@ -62,9 +67,31 @@ class OrderService
         return $identify;
     }
 
+    private function getProductsByOrder(array $productsOrder): array
+    {
+        $products = [];
+        foreach ($productsOrder as $productOrder) {
+            $product = $this->productRepository->getProductByUuid($productOrder['identify']);
+
+            array_push($products, [
+                'id' => $product->id,
+                'qty' => $productOrder['qty'],
+                'price' => $product->price,
+            ]);
+        }
+
+        return $products;
+    }
+
+
     public function getTotalOrder(array $products): float
     {
-        return (float) 90;
+        $total = 0;
+
+        foreach ($products as $product) {
+            $total += ($product['price'] * $product['qty']);
+        }
+        return (float) $total;
     }
 
     private function getTenantIdByOrder(string $uuid)
