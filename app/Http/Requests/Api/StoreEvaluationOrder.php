27ae1ca1b87f2692f5a45163f64af68d71dabba2
rodrigompais <59613110+rodrigompais@users.noreply.github.com>
2022-08-25 18:42:14 -0300

    API NO LARAVEL - VALIDAÇÕES DE AVALIAÇÃO
    API NO LARAVEL - VALIDAÇÕES DE AVALIAÇÃO

--- app/Http/Requests/Api/StoreEvaluationOrder.php
+++ app/Http/Requests/Api/StoreEvaluationOrder.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Api;
 
+use App\Repositories\Contracts\OrderRepositoryInterface;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreEvaluationOrder extends FormRequest
@@ -13,7 +14,15 @@ class StoreEvaluationOrder extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        if (!$client = auth()->user()) {
+            return false;
+        }
+
+        if (!$order = app(OrderRepositoryInterface::class)->getOrderByIdentify($this->identifyOrder)) {
+            return false;
+        };
+
+        return $client->id == $order->client_id;
     }
 
     /**
@@ -24,8 +33,8 @@ class StoreEvaluationOrder extends FormRequest
     public function rules()
     {
         return [
-            'stars' => ['required', 'integer','min:1','max:5'],
-            'comment' => ['nullable', 'min:3','max:1000'],
+            'stars' => ['required', 'integer', 'min:1', 'max:5'],
+            'comment' => ['nullable', 'min:3', 'max:1000'],
         ];
     }
 }
