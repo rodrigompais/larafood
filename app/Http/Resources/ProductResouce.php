2718de27e58265dd5a8049cb53d5865c85669763
rodrigompais <59613110+rodrigompais@users.noreply.github.com>
2022-08-26 22:56:26 -0300

    MELHORAR RESOURCES API LARAVEL
    MELHORAR RESOURCES API LARAVEL

--- app/Http/Resources/ProductResouce.php
+++ app/Http/Resources/ProductResouce.php
@@ -17,6 +17,7 @@ class ProductResouce extends JsonResource
         return [
             'identify' => $this->uuid,         
             'flag' => $this->flag,
+            'title' => $this->title,
             'image' => url ("storage/{$this->image}"),
             'price' => $this->price,
             'description' => $this->description,
